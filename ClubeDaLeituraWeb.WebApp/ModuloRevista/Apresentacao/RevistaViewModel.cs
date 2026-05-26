98c84ac1ebef990f8e668d78aab63cd25c3967ef
Michel <michelgabiatti07@gmail.com>
2026-05-26 12:04:41 -0300

    Implementacao RevistaViewModel

---
+++ ClubeDaLeituraWeb.WebApp/ModuloRevista/Apresentacao/RevistaViewModel.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using ClubeDaLeituraWeb.WebApp.ModuloCaixa.Dominio;
+
+namespace ClubeDaLeituraWeb.WebApp.ModuloRevista.Apresentacao;
+
+public record RevistaViewModel
+(
+    [Required(ErrorMessage = "O campo \"Titulo\" deve ser preenchido.")]
+    [StringLength(100, MinimumLength = 2, ErrorMessage = "O campo \"Titulo\" deve conter no máximo 100 e no minimo 2 caracteres.")]
+    string Titulo,
+
+    [Range(1, int.MaxValue, ErrorMessage = "O campo \"NumeroDeEdicao\" deve ser positivo (maior que zero).")]
+    uint NumeroDeEdicao,
+
+    [Range(1600, 2100, ErrorMessage = "O campo \"Dias de Empréstimo\" deve ser um ano valido")]
+    int AnoDePublicacao,
+
+    Caixa? Caixa = null,
+
+    string Id = ""
+);
